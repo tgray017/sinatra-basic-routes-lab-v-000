f616767ad31598eceeb63be0b536fca79d8a46c5
Thomas Gray <tgray017@gmail.com>
2019-04-01 12:37:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is Tom"
   end
+  
+  get '/hometown' do 
+    "My hometown is Lemont, PA"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'Wait So Long'"
+  end
 end
