31c49d337f7f792f9b2f10b50a11c22710046e96
Thomas Gray <tgray017@gmail.com>
2019-04-01 12:38:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Tom"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Lemont, PA"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'Wait So Long'"
+  end
 end
